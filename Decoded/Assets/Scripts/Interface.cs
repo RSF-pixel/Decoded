0aeff882f74774cfb6100283eb6939da020ddd78
Cheap-Ship <56252676+Cheap-Ship@users.noreply.github.com>
2021-06-08 20:27:27 +0100

    Timer, Paused, GameOver, & Completed

---
+++ Decoded/Assets/Scripts/Interface.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Interface : MonoBehaviour
+{
+    public static bool showPanel = false;
+    public static string showText = "";
+    [SerializeField] GameObject panel;
+    [SerializeField] Text text;
+
+    void Update()
+    {
+        panel.SetActive(showPanel);
+        text.text = showText;
+        Cursor.lockState = !showPanel ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = showPanel;
+        Time.timeScale = System.Convert.ToSingle(!showPanel);
+    }
+
+    public void Restart()
+    {
+        showPanel = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Decoded/Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private bool countTime = false;
+    private float timer = 0f;
+    [SerializeField] Text clock;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Start") && !countTime)
+        {
+            countTime = true;
+        } 
+        else if (other.CompareTag("Finish") && countTime)
+        {
+            Interface.showPanel = true;
+            Interface.showText = "DEMO COMPLETE";
+            countTime = false;
+        }
+    }
+
+    void Update()
+    {
+        ShowTime(timer);
+        if (countTime)
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    void ShowTime(float time)
+    {
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+        float milliseconds = Mathf.FloorToInt((time*1000)%1000);
+        clock.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+    }
+}
